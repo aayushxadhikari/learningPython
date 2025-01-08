f6be4445853fab2df186524633bf43b5efa5889f
aayushxadhikari <aayushxadhikari@gmail.com>
2025-01-08 22:03:40 +0545

    linked list

--- LinkedList/LL_constructor.py
+++ LinkedList/LL_constructor.py
@@ -25,9 +25,27 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
         return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -=1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.value
+
         
 my_linked_list = LinkedList(4)
 my_linked_list.append(1)
 my_linked_list.append(3)
-
+print(my_linked_list.print_list())
+print(my_linked_list.pop())
 print(my_linked_list.print_list())
