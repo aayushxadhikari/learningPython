05325e95ac75ab22ab9f3d80924a93aa8a1293c4
aayushxadhikari <aayushxadhikari@gmail.com>
2025-01-15 06:32:42 +0545

    fixed code

--- LinkedList/LL_constructor.py
+++ LinkedList/LL_constructor.py
@@ -2,61 +2,59 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
 class LinkedList:
     def __init__(self, value):
-        new_node = Node(value)  # Create a new node with the given value
-        self.head = new_node    # Set the head pointer to this new node
-        self.tail = new_node    # Set the tail pointer to this new node
-        self.length = 1         # Set the length of the list to 1
-
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
 
     def print_list(self):
-         temp = self.head  # Start traversal from the head node
-         while temp is not None:  # Continue until you reach the end of the list
-            print(temp.value)  # Print the value of the current node
-            temp = temp.next   # Move to the next node
+        temp = self.head
+        while temp is not None:
+            print(temp.value, end=" -> ")
+            temp = temp.next
+        print("None")
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
 
-    def append(self, value): # adding something 
-        new_node = Node(value)  # Create a new node with the given value
-        if self.head is None:   # If the list is empty
-         self.head = new_node  # Set the new node as both head and tail
-         self.tail = new_node
-        else:                   # If the list is not empty
-            self.tail.next = new_node  # Link the current tail to the new node
-            self.tail = new_node       # Update the tail pointer to the new node
-        self.length += 1         # Increment the length of the list
-        return True              # Return True to indicate success
-
-    
     def pop(self):
-        if self.length == 0:    # If the list is empty, return None
-          return None
-        temp = self.head        # Start traversal from the head
-        pre = self.head         # Use `pre` to track the second-to-last node
-        while(temp.next):       # Traverse until the last node
-          pre = temp          # Update `pre` to the current node
-          temp = temp.next    # Move `temp` to the next node
-        self.tail = pre         # Update the tail to point to the second-to-last node
-        self.tail.next = None   # Disconnect the last node
-        self.length -= 1        # Decrease the length
-        if self.length == 0:    # If the list becomes empty
-         self.head = None    # Reset head and tail
-         self.tail = None
-        return temp.value       # Return the value of the removed node
-    
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.value
 
-    def prepend(self, value): #adding something to the start 
-       new_node = Node(value)
-       if self.length == 0:
-          self.head = new_node
-          self.tail = new_node
-       else:
-          new_node.next = self.head
-          self.head = new_node
-       self.length += 1
-       return True
-    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
     def pop_first(self):
         if self.length == 0:
             return None
@@ -66,27 +64,28 @@ class LinkedList:
         self.length -= 1
         if self.length == 0:
             self.tail = None
-        return temp
-    
+        return temp.value
+
     def get(self, index):
-       if index < 0 or index >= self.length:
-          return None
-       for _ in range(index):
-          temp = temp.next
-       return temp.value
-    
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp.value
 
-       
-          
-
-
-        
+# Testing the corrected code
 my_linked_list = LinkedList(4)
 my_linked_list.append(1)
 my_linked_list.append(3)
 my_linked_list.prepend(6)
 my_linked_list.print_list()
-# print(my_linked_list.print_list())
-# print(my_linked_list.pop())
-# print(my_linked_list.print_list())
-# print(my_linked_list.prepend())
+
+# Testing other methods
+print("Popped value:", my_linked_list.pop())
+my_linked_list.print_list()
+
+my_linked_list.prepend(10)
+my_linked_list.print_list()
+
+print("Value at index 2:", my_linked_list.get(2))
